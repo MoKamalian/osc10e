13fbc1a40d03cdec27cd2d57a48a8077166d5c32
Ubuntu_2022 <am445669@dal.ca>
2022-03-19 15:15:22 -0700

    chapter 3 project 1

--- ch3/simple-shell.c
+++ ch3/simple-shell.c
@@ -5,27 +5,33 @@
  * Copyright John Wiley & Sons - 2018
  */
 
+
+
 #include <stdio.h>
 #include <unistd.h>
 
-#define MAX_LINE		80 /* 80 chars per line, per command */
+/* the maximum chars for the command is 80 */
+#define MAX_LINE 80 
+/* this is the main program that will run to ask the user for input in the 
+* command line terminal */
+int main(void) {
+	char *args[MAX_LINE / 2 + 1];
+	int should_run = 1;
+	/* as long as should_run is 1 the program will continue to ask user for input */
+	while(should_run) {
+		printf("osh>");
+		fflush(stdout);
 
-int main(void)
-{
-	char *args[MAX_LINE/2 + 1];	/* command line (of 80) has max of 40 arguments */
-    	int should_run = 1;
-		
-    	while (should_run){   
-        	printf("osh>");
-        	fflush(stdout);
-        
-        	/**
+		/**
          	 * After reading user input, the steps are:
          	 * (1) fork a child process
          	 * (2) the child process will invoke execvp()
          	 * (3) if command included &, parent will invoke wait()
          	 */
-    	}
-    
+		
+	} 
+
+	//execvp(char *command, char params[]);
+
 	return 0;
 }
