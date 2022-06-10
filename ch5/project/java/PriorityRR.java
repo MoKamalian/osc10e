9209d3252da218be75f8bda7043ad83436e57c3b
mo-kamalian <am445669@dal.ca>
2022-06-10 20:32:19 -0300

    priority rr scheduler added, ch5 programming project finished

--- ch5/project/java/PriorityRR.java
+++ ch5/project/java/PriorityRR.java
@@ -1,10 +1,8 @@
 /*
  *
- * 
- * 
- * 
- * 
- * 
+ * Author: amir kamalian
+ * Date: 10-June-2022
+ * Description: Priority RR scheduler.  This is non-preemptive 
  * 
  * 
  */
@@ -15,18 +13,45 @@ import java.util.List;
 
 public class PriorityRR implements Algorithm {
  
-    public PriorityRR(List<Task> queue) {
 
+    private List<Task> queue;
+
+
+    public PriorityRR(List<Task> queue) {
+        this.queue = queue; 
     }
 
 
     public void schedule() {
 
+        pickNextTask();
+        for(Task task : queue) {
+            CPU.run(task, task.getBurst());
+        }
+
     }
 
 
     public Task pickNextTask() {
+        
+        Task temp;
+        for(int i=0; i<queue.size(); i++) {
 
+            for(int j=i+1; j<queue.size(); j++) {
+
+                if(queue.get(i).getPriority() > queue.get(j).getPriority()) {
+
+                    temp = queue.get(i);
+                    queue.set(i, queue.get(j));
+                    queue.set(j, temp);
+
+                }
+
+            }
+
+        }
+
+        return queue.get(0);
     }
 
 
