b682b3d9884eaa3711d1368f70e652f4c4fb85c8
Mo Kamalian <am445669@dal.ca>
2023-01-23 19:39:16 -0400

    chapter 9 project initial commit

--- ch9/main.c
+++ ch9/main.c
@@ -17,8 +17,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
 
-#define MEMSIZE int 
+#define MEMSIZE u_int32_t  
 
 /* valid commands list */
 typedef enum Commands {
@@ -31,10 +33,40 @@ typedef enum Commands {
 
 int main(int argc, char** argv) {
 
+    /* memory size validation: still incomplete but good enough 
+    for project purposes (no validation for 123a555) */
+    if(argc > 2) {
+        printf("[too many arguments passed]");
+        return 1;
+    } else if(argc < 1) {
+        printf("[no arguments given]");
+        return 1; 
+    }
+
+    int sizeCheck = atoi(argv[1]);
+    if(!isdigit(argv[1][0])) {
+        printf("[must enter numeric number]");
+        return 1; 
+    }
+
+    if(sizeCheck < 0) {
+        printf("[Non-positive integer given]");
+        return 1; 
+    }
+    
 
     /* parse for the memory size given to the program */
     MEMSIZE memory = (int) strtol(argv[1], (char **)NULL, 10);
+    printf("%i bytes of memory have been allocated\n", memory);
 
+    /* main program loop */
+    bool programEnd = false;
+    while(!programEnd) {
+        
+        printf("allocator>");
+
+
+    }
 
 
     return 0; 
